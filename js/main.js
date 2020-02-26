13c1f96f9c3d6868c8d121df6b758b0cfe27972c
Fred Zou <xmfc1912@gmail.com>
2020-02-26 23:12:33 +0800

    one page webite by flexbox

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+// navbar background - 会导致点NAVBAR的标签时, 跳转后的内容标题会被导航条所覆盖. 所以先注释掉;
+
+// window.addEventListener('scroll', function() {
+//   if (window.scrollY > 150) {
+//     this.document.querySelector('#navbar').style.opacity = 0.9;
+//   } else {
+//     this.document.querySelector('#navbar').style.opacity = 1
+//   }
+// });
+
